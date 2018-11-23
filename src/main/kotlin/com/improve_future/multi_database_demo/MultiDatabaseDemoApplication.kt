dd87333e6f8b2c777db9d6738f4fbf723b0a2155
Kenji Ohtsuka <kok.fdcm@gmail.com>
2018-11-23 20:34:42 +0900

    initial

---
+++ src/main/kotlin/com/improve_future/multi_database_demo/MultiDatabaseDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.improve_future.multi_database_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MultiDatabaseDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<MultiDatabaseDemoApplication>(*args)
+}
